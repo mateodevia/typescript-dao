b8d25f6ad502529afc3756d69634ca88613ade0a
mateodevia <MY_NAME@example.com>
2022-12-12 08:01:02 -0500

    Added getShareHolders logic to smart contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract Token is ERC20Votes {
+    address[] private tokenHolders;
+
     constructor(uint256 _initialSupply)
         ERC20("Token", "T")
         ERC20Permit("Token")
@@ -30,5 +32,62 @@ contract Token is ERC20Votes {
         override(ERC20Votes)
     {
         super._burn(account, amount);
+    }
+
+    /**
+     * Overrides the transfer function to keep tract of the addresses that have tokens
+     */
+    function transfer(address to, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool)
+    {
+        // Make the transfer
+        bool res = super.transfer(to, amount);
+
+        // Update the tokenHolders
+
+        /** The sender in which the to address is found in the tokenHolders array */
+        uint256 senderIndex;
+        /** Determines if the sender address was found in the tokenHolders array */
+        bool senderWasFound = false;
+
+        /** The index in which the to address is found in the tokenHolders array */
+        uint256 receiverIndex;
+        /** Determines if the to address was found in the tokenHolders array */
+        bool receiverWasFound = false;
+
+        // Look for the sender and the receiver in the tokenHolders array
+        for (uint256 i = 0; i < tokenHolders.length; i++) {
+            if (tokenHolders[i] == msg.sender) {
+                senderIndex = i;
+            }
+            if (tokenHolders[i] == to) {
+                receiverIndex = i;
+                receiverWasFound = true;
+            }
+        }
+
+        uint256 senderBalanceAfter = balanceOf(msg.sender);
+
+        // If the sender has 0 tokens then it should be removed from the tokenHolders array
+        if (senderWasFound && senderBalanceAfter == 0) {
+            for (uint256 i = receiverIndex; i < tokenHolders.length - 1; i++) {
+                tokenHolders[i] = tokenHolders[i + 1];
+            }
+            tokenHolders.pop();
+        }
+
+        // In the receiver was not found in the tokenHolders array, it should be added because has tokens now
+        if (!receiverWasFound) {
+            tokenHolders.push(to);
+        }
+
+        return res;
+    }
+
+    function getTokenHolders() public view returns (address[] memory) {
+        return tokenHolders;
     }
 }
--- scripts/api/users.ts
+++ scripts/api/users.ts
@@ -7,15 +7,5 @@ import { Token } from "../../typechain";
 export const getShareHolders = async (contracts: {
   token: Token;
 }): Promise<string[]> => {
-  return Promise.all(
-    // As as MVP the 5 hardhat addresses are hard coded.
-    // In the future the Token contract should keep track of the address that hold tokens and therefore have voting power
-    [
-      "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-      "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
-      "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
-      "0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65",
-      "0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc",
-    ].map((add) => add)
-  );
+  return await contracts.token.getTokenHolders();
 };
